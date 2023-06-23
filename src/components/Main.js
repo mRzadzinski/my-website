fc041fc769a6e037f34c92b24aa8ede25d05f19c
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-06-23 20:06:36 +0200

    Create Main page component

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,15 @@ import { Link } from 'react-router-dom';
 import '../styles/Main.scss';
 
 const Main = () => {
-	return <div className='Main'></div>;
+	return (
+		<div className='Main'>
+			<div className="intro">
+				I’m Maciej Rządziński - Front End Developer. I’m excited about new
+				technologies and making the world more interactive.
+			</div>
+			<div className="more-about-me">more about me</div>
+		</div>
+	);
 };
 
 export default Main;
