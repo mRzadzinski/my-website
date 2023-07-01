4e17c1853600063b242cdfe59116ae647457fec4
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-07-01 14:30:40 +0200

    Style Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,11 +10,34 @@ const Contact = () => {
 
 	return (
 		<div className='Contact'>
-			rzadzinski.maciej@gmail.com
-			<br />
-			github
-			<br />
-			linkedin
+			<div className='mail-contact'>
+				mail me <span id='mail-contact'>rzadzinski.maciej@gmail.com</span>
+			</div>
+
+			<div className='see-my-icons'>
+				see my
+				<div className='github'>
+					<img
+						onClick={() => openInNewTab('https://github.com/mRzadzinski')}
+						className='social-media-icons-contact'
+						src={githubIcon}
+						alt='github-icon'
+					/>
+				</div>
+				<div className='linkedin'>
+					<img
+						onClick={() =>
+							openInNewTab(
+								'https://pl.linkedin.com/in/maciej-rz%C4%85dzi%C5%84ski-28450420b'
+							)
+						}
+						id='linkedin-icon-contact'
+						className='social-media-icons-contact'
+						src={linkedinIcon}
+						alt='linkedin-icon'
+					/>
+				</div>
+			</div>
 		</div>
 	);
 };
