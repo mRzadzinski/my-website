48adc9e4e0cba2c8a134ed8c0fcb591012536160
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-08-03 22:27:50 +0200

    Make About responsive

--- src/components/About.js
+++ src/components/About.js
@@ -1,42 +1,45 @@
 import React from 'react';
 import '../styles/About.scss';
-import myFace from '../img/my-face.jpg';
+import myFaceLg from '../img/my-face-lg.jpg';
+import myFaceMd from '../img/my-face-md.jpg';
+import myFaceSm from '../img/my-face-sm.jpg';
 
 const About = () => {
 	return (
 		<div className='About'>
 			<div className='all-about-me'>
-				<div className='about-content'>
-					Challenges have always excited me - I cycled <nobr>a distance</nobr>{' '}
-					of 240 km (149 mi) in a single day, ran a half marathon{' '}
-					<nobr>(1 h 44 min)</nobr> and can do some weird yoga asanas. But it
-					turned out that solving logical, programmatic problems can be as
-					addictive as post-heavy-workout high.
-					<br />
-					<br />
-					So I embarked on an exciting journey and decided to become a software
-					developer.
-					<br />
-					<br />
-					Let's look at some recent facts:
-					<br />
-					<ul>
-						<li>
-							I completed Harvard CS50 Course and JavaScript Path from The Odin Project
-							Course.
-						</li>
-						<li>
-							In my last job I was promoted twice, up to project coordinator.
-						</li>
-						<li>I improved my English from B2 to C1.</li>
-						<li>Learned touch typing.</li>
-						<li>Practice playing guitar five days a week.</li>
-					</ul>
-					All of these things require effort, good organization and consistent
-					execution <nobr>- that’s what you can expect of me.</nobr>
-				</div>
+				<picture>
+					<source media='(max-width: 480px)' srcSet={myFaceSm} />
+					<source media='(max-width: 950px)' srcSet={myFaceMd} />
+					<img id='my-face' src={myFaceLg} alt='my-face' />
+				</picture>
 
-				<img id='my-face' src={myFace} alt='my-face' />
+				<div className='about-content'>
+					<div>
+						Challenges have always excited me - I cycled <nobr>a distance</nobr>{' '}
+						of 240 km (149 mi) in a single day, ran a half marathon{' '}
+						<nobr>(1 h 44 min)</nobr> and can do some weird yoga asanas. But it
+						turned out that solving logical, programmatic problems can be as
+						addictive as post workout endorphins rush.
+						<br />
+						<br />
+						So I embarked on an exciting journey and decided to become a software
+						developer.
+						<br />
+						<br />
+						Let's look at some things I've been doing recently:
+						<br />
+						<ul>
+							<li>Completed Harvard CS50 Course</li>
+							<li>Completed Frontend JS Path from The Odin Project Course</li>
+							<li>Improved my English from B2 to C1</li>
+							<li>Learned touch typing</li>
+							<li>Learn to play guitar</li>
+						</ul>
+						All of these things require effort, good organization and consistent
+						execution <nobr>- that’s what you can expect of me.</nobr>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
