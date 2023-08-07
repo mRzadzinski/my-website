9481c69667e53866d67da07f625150c518896cab
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-08-07 18:11:40 +0200

    Make Skills responsive

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,22 +11,22 @@ const Skills = () => {
 				<Icon
 					icon='dashicons:html'
 					color='rgb(174 83 83)'
-					width='60'
-					height='60'
+					width='50'
+					height='50'
 				/>
-				<Icon icon='vaadin:css' color='#1f72b6' width='60' height='60' />
-				<Icon icon='logos:sass' width='60' height='60' />
-				<Icon icon='logos:javascript' width='60' height='60' />
-				<Icon icon='devicon:react-wordmark' width='70' height='70' />
-				<Icon icon='logos:git' width='70' height='70' />
-				<Icon icon='logos:github' width='70' height='70' />
+				<Icon icon='vaadin:css' color='#1f72b6' width='45' height='45' />
+				<Icon icon='logos:sass' width='45' height='45' />
+				<Icon icon='logos:javascript' width='45' height='45' />
+				<Icon icon='devicon:react-wordmark' width='45' height='45' />
+				<Icon icon='logos:git' width='55' height='55' />
+				<Icon icon='logos:github' width='55' height='55' />
+				<img id='jest-icon' src={jestIcon} alt='jest' />
 				<Icon
 					id='webpack-icon'
 					icon='devicon:webpack-wordmark'
 					width='130'
 					height='130'
-				/>
-				<img id='jest-icon' src={jestIcon} alt='jest' />
+				/>				
 			</div>
 
 			<div className='skill-title'>Languages</div>
